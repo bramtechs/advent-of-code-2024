9416d5632ad9ac1ff747408cdb498cd78e3b7e12
bramtechs <bramtech@telenet.be>
2024-12-02 20:39:51 +0100

    Day 02 - Part 02

--- src/day02.rs
+++ src/day02.rs
@@ -2,6 +2,20 @@
 
 use std::fs::read_to_string;
 
+fn is_report_safe(report: &Vec<i32>) -> bool {
+    // all increasing or decreasing
+    if report.is_sorted() || report.is_sorted_by(|a, b| a >= b) {
+        for n in 0..(report.len() - 1) {
+            let diff = report[n].abs_diff(report[n + 1]);
+            if diff < 1 || diff > 3 {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
 pub fn solve(path: &str) {
     let levels: Vec<Vec<i32>> = read_to_string(path)
         .expect(format!("Failed to read {}", path).as_str())
@@ -17,22 +31,28 @@ pub fn solve(path: &str) {
         })
         .collect();
 
-    let safe: Vec<Vec<i32>> = levels
+    let safe_part1: Vec<Vec<i32>> = levels.clone().into_iter().filter(is_report_safe).collect();
+    let safe_part2: Vec<Vec<i32>> = levels
+        .clone()
         .into_iter()
         .filter(|report| {
-            // all increasing or decreasing
-            if report.is_sorted() || report.is_sorted_by(|a, b| a >= b) {
-                for n in 0..(report.len() - 1) {
-                    let diff = report[n].abs_diff(report[n + 1]);
-                    if diff < 1 || diff > 3 {
-                        return false;
-                    }
-                }
+            if is_report_safe(report) {
                 return true;
+            }
+            for i in 0..report.len() {
+                let mut short_report = report.clone();
+                short_report.remove(i);
+                if is_report_safe(&short_report) {
+                    return true;
+                }
             }
             return false;
         })
         .collect();
 
-    println!("Day 02 - Part 01 - Safe reports: {}", safe.len())
+    println!("Day 02 - Part 01 - Safe reports: {}", safe_part1.len());
+    println!(
+        "Day 02 - Part 02 - Safe reports allowing one mistake: {}",
+        safe_part2.len()
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub mod day01;
+pub mod day02;
 
 fn main() {
     day01::solve("assets/day01.txt");
+    day02::solve("assets/day02.txt");
 }
